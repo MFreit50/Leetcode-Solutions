efd151392d8890bfc7260aee01d0e3102d8cfa68
MFreit50 <93632207+MFreit50@users.noreply.github.com>
2024-01-30 16:31:43 -0500

    Checkstyle test
    changed bracket placement and added bracketless if statement

--- Java/1_TwoSum.java
+++ Java/1_TwoSum.java
@@ -1,9 +1,10 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-    
+    public int[] twoSum(int[] nums, int target)
+    {
+        if(1 == 1)
+            System.out.println("checkstyle test");
         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
         for(int i = 0; i < nums.length; i++ ){
-
             int complement = target - nums[i];
             if(hm.containsKey(complement)){
                 return new int[] {hm.get(complement),i};
