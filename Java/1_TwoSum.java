b5542dcb7a4de470530209af280490d66b070c14
MFreit50 <93632207+MFreit50@users.noreply.github.com>
2024-01-30 16:33:04 -0500

    Update 1_TwoSum.java

--- Java/1_TwoSum.java
+++ Java/1_TwoSum.java
@@ -1,8 +1,5 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target)
-    {
-        if(1 == 1)
-            System.out.println("checkstyle test");
+    public int[] twoSum(int[] nums, int target){
         HashMap<Integer,Integer> hm = new HashMap<Integer,Integer>();
         for(int i = 0; i < nums.length; i++ ){
             int complement = target - nums[i];
